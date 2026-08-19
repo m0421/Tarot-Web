53fbedf1252e8cfd47cb77ccb528a81362dacb06
m0421 <759225702@qq.com>
2026-08-20 02:19:09 +0800

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,8 +1,7 @@
 <template>
   <footer class="w-full max-w-[888px] px-[16.66px] mt-8 pb-8">
     <main class="m-auto pt-4 flex flex-col-reverse md:flex-row justify-between gap-6 md:gap-16 w-full max-w-[1666px] border-t">
-      <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left [&>p>a]:text-slate-400">
-        <p>塔罗牌占卜 由 <a href="https://www.vvhan.com" target="_blank" title="韩小韩博客">韩小韩博客</a> 提供技术支持</p>
+      <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left [&>p>a]:text-slate-400"> 
         <p>开源地址 <a href="https://github.com/uxiaohan/Tarot-Web" target="_blank" title="Tarot-Web">Tarot-Web</a></p>
       </section>
       <section class="flex flex-col items-center gap-2">
